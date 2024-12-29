4aec1bfd698ba9327085965533c34b01335de603
techno-sam <77073745+techno-sam@users.noreply.github.com>
2024-12-29 01:31:54 -0800

    embree 4

--- distfiles/embree-install.sh
+++ distfiles/embree-install.sh
@@ -6,10 +6,10 @@ LIBRARY_INSTALL_DIR="$1"/lib
 mkdir -p $INCLUDE_INSTALL_DIR
 mkdir -p $LIBRARY_INSTALL_DIR
 
-cp -av include/embree3 $INCLUDE_INSTALL_DIR
+cp -av include/embree4 $INCLUDE_INSTALL_DIR
 
 cp -av lib/lib* $LIBRARY_INSTALL_DIR
 
 rm $LIBRARY_INSTALL_DIR/libtbb.so
-rm $LIBRARY_INSTALL_DIR/libtbb.so.12
-cp $LIBRARY_INSTALL_DIR/libtbb.so.12.1 $LIBRARY_INSTALL_DIR/libtbb.so.12
+#rm $LIBRARY_INSTALL_DIR/libtbb.so.12
+#cp $LIBRARY_INSTALL_DIR/libtbb.so.12.9 $LIBRARY_INSTALL_DIR/libtbb.so.12
