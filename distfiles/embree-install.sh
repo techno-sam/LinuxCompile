da0afeee369d47aa44181cd0a2547661e0decec3
techno-sam <77073745+techno-sam@users.noreply.github.com>
2025-01-01 15:58:42 -0800

    Might need that library, actually

--- distfiles/embree-install.sh
+++ distfiles/embree-install.sh
@@ -11,6 +11,5 @@ cp -av include/embree4 $INCLUDE_INSTALL_DIR
 cp -av lib/lib* $LIBRARY_INSTALL_DIR
 
 rm -v $LIBRARY_INSTALL_DIR/libtbb.so
-rm -v $LIBRARY_INSTALL_DIR/libtbb.so.2
 rm -v $LIBRARY_INSTALL_DIR/libtbb.so.12
 cp -v $LIBRARY_INSTALL_DIR/libtbb.so.12.11 $LIBRARY_INSTALL_DIR/libtbb.so.12
--- distfiles/oidn-install.sh
+++ distfiles/oidn-install.sh
@@ -11,7 +11,6 @@ cp -av include/OpenImageDenoise $INCLUDE_INSTALL_DIR
 cp -av lib/lib* $LIBRARY_INSTALL_DIR
 
 rm -v $LIBRARY_INSTALL_DIR/libtbb.so
-rm -v $LIBRARY_INSTALL_DIR/libtbb.so.2
 rm -v $LIBRARY_INSTALL_DIR/libtbb.so.12
 cp -v $LIBRARY_INSTALL_DIR/libtbb.so.12.11 $LIBRARY_INSTALL_DIR/libtbb.so.12
 
